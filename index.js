0e415f50d5373835de675fa88185e736b37324d3
TonyChouteau <tony.chouteau@outlook.fr>
2019-10-03 17:11:04 +0200

    utf8

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+/*
+    Variables definition
+*/
+
+let player = null;
+let others = [];
+
+let planets = [];
+
+/*const request = async() => {
+    const response = await fetch("./data.json");
+    const json = await response.json();
+    return json
+}
+
+console.log("Loading");
+let data = null;
+data = request();
+while (data === null) {}
+console.log("Done");*/
+
+/*
+    END Variables definition
+*/
+
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     //canvas.parent("canvas-div");
@@ -9,6 +34,7 @@ function windowResized() {
     //canvas.parent("canvas-div");
 }
 
+
 function draw() {
-    background(255, 0, 0);
+    background(0, 0, 0);
 }
